5a7273ec40fd03a297f428650f2d0799705f9519
bishantraj <bishantraj49@gmail.com>
2026-02-24 12:22:59 +0530

    variable assignment

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 ﻿console.log("introduction to variables in JS");
+// variable
+// var name ="Ayush";
+// var surname="pandey";
+// console.log(name);
+// console.log(surname);
+//let variable
+// let name="ayush";
+// name="ayushpandey";
+// console.log(name);
+// {
+//     let a=10;
+//     let b =20;
+//     let sum=a+b;
+//     console.log(sum);
+
+// console.log(a);
+//}
+//const variable    
+
+    const dv {
+        Key:Value;
+        Name:"Ayush";
+        Section:"CSE";
+        College:"Abes"
+    
+        gconsole.log(dv.Name);
+    }
